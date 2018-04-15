65661443c8b27bf32e86739f6825c6fe9425896c
Marcos Monteiro <marcosmmb90@gmail.com>
2018-04-15 09:20:15 -0300

    last

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn raiek.wsgi --log-file -
--- raiek/settings.py
+++ raiek/settings.py
@@ -118,14 +118,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
